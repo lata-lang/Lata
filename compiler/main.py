3a8944ef1bcd13ef1a5ac02d086795ad46fd556f
lop.py <100444985+lop-p@users.noreply.github.com>
2022-06-04 21:58:19 -0500

    compiler made

---
+++ compiler/main.py
@@ -0,0 +1,18 @@
+def main():
+    # get the file name
+    fileName = input("Enter the file name: ")
+    # open the file
+    file = open(fileName, "r")
+    # check lines and shit
+    for line in file:
+        if line.find("//") != -1:
+            line = line.replace("//", "#")
+        if line.find("@python") != -1:
+            line = line.replace("@python", "")
+        if line.find("@") != -1:
+            line = line.replace("@", "")
+        if line.find("\n") != -1:
+            line = line.replace("\n", "")
+        print("lata. " + line)
+    # close the file
+    file.close()
