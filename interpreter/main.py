1a7b93ee249fecac962d7798968ad07f5a11a5f3
ohio-hampter <williamds@madisoncity.k12.al.us>
2022-06-05 14:20:01 -0500

    modified .gitignore and added an error hint to the semicolons

--- interpreter/main.py
+++ interpreter/main.py
@@ -1,14 +1,15 @@
 import sys
 
 
-
 def main():
     # get the file name
     fileName = sys.argv[1]
     # open the file
     file = open(fileName, "r")
     # actual interpreter
+    linfes = 0
     for line in file:
+        linfes += 1
         if "write" in line:
             print(line[line.find("write")+6:])
         if "//" in line:
@@ -17,6 +18,14 @@ def main():
             input()
         if ";" in line:
             print("Error: Contains semicolon")
+            print("Hint: Don't use semicolons. It's not that hard.")
+            print(" --------------------------------------------------")
+            print("|  print(Hello, Latty!;                            |")
+            print("|        _____________^_                           |")
+            print("|       |EW. SEMICOLONS.|                          |")
+            print("|       |_______________|                          |")
+            print(" --------------------------------------------------")
+            exit()
     # close the file
     file.close()
 
