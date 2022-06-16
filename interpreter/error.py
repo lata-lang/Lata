0210af6133ba01534e24d0abd622baea00bebc75
Doane Tsu <100444985+lop-p@users.noreply.github.com>
2022-06-15 19:50:22 -0500

    calliDs exist

---
+++ interpreter/error.py
@@ -0,0 +1,11 @@
+def error(calliD):
+    print("Error: " + calliD)
+    if calliD == "1semi":
+        print("You can't have semicolons.")
+        print("Example of bad code:")
+        print("----------------------------")
+        print("|   varstr var1 = coffin;  |") 
+        print("----------------------------")
+    exit()
+    # this is the error function
+
