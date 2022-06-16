37df33e2a347ddc66061cc03077d9171f7ca78d6
Doane Tsu <100444985+lop-p@users.noreply.github.com>
2022-06-15 19:46:09 -0500

    making the error check

--- interpreter/main.py
+++ interpreter/main.py
@@ -10,25 +10,37 @@ def main():
     linfes = 0
     for line in file:
         linfes += 1
+        if "varstr" in line:
+            # i guess declare a variable?
+            varName = line.split(" ")[1]
+            # get the value
+            varValue = line.split(" ")[2]
+
+            varRef = "<ref>" + varName + "</ref>"
+            # this won't be able to be refrenced in future code 
+            # so i'll leave it here for the future
+       
+        
         if "write" in line:
-            print(line[line.find("write")+6:])
+            if varRef in line:
+                # this prints out the value of
+                print(varValue)
+            # this works much better
+            print(line[line.find(".")+1:], end="")
+            
         if "//" in line:
             continue
         if "read" in line:
-            input(line[line.find("read")+5:])
+            print(line[line.find(".")+1:], end="")
         if "@py" in line:
             # python interop
             exec(line[line.find("@py")+3:])
-        if ";" in line:
-            print("Error: Contains semicolon")
-            print("on line: " + str(linfes))
-            print(" --------------------------------------------------")
-            print("|  print(Hello, Latty!;                            |")
-            print("|              _______^_                           |")
-            print("|             |Semicolon|                          |")
-            print("|             |_________|                          |")
-            print(" --------------------------------------------------")
+        # i'll have an error check in the future that runs before the interpreter
+        # additional commands
+        if "exit" in line:
             exit()
+
+
     # close the file
     file.close()
 
